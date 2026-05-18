9b4c617660eac390304dbeb3301dcefe2b161c8b
candratama <wahyucandratama@gmail.com>
2026-05-18 22:38:46 +0700

    feat(main): wire program with preflight checks

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/candratama/sshm/internal/config"
+	"github.com/candratama/sshm/internal/password"
+	"github.com/candratama/sshm/internal/ui"
+)
 
 func main() {
-	fmt.Println("sshm")
+	if err := preflight(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	path, err := config.DefaultPath()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "home dir:", err)
+		os.Exit(1)
+	}
+	store, err := config.Load(path)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "load config:", err)
+		os.Exit(1)
+	}
+
+	app := ui.NewApp(store, password.New(), path)
+	p := tea.NewProgram(app, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, "tea:", err)
+		os.Exit(1)
+	}
+}
+
+func preflight() error {
+	if _, err := exec.LookPath("pass"); err != nil {
+		return fmt.Errorf("pass not installed, run: brew install pass")
+	}
+	if _, err := exec.LookPath("sshpass"); err != nil {
+		return fmt.Errorf("sshpass not installed, run: brew install hudochenkov/sshpass/sshpass")
+	}
+	return nil
 }
